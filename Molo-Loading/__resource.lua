490751caff5fa0316e4862019ea070f3f650bd7d
MoloMort <88444392+MoloMort@users.noreply.github.com>
2021-08-04 16:29:07 +0200

    Add files via upload

---
+++ Molo-Loading/__resource.lua
@@ -0,0 +1,18 @@
+files {
+    'index.html',
+    'style.css',
+    'logo.png',
+    'background.jpg',
+    'music/Loading.ogg',
+}
+
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+
+
+
+
+-- [[LAVET AF MOLO TJEK GERNE SHOPPEN UD PÅ DETTE LINK --]]
+--            [[discord.gg/Txu6yAvGNa]]
